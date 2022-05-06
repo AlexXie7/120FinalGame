6066eb655bf12914e5ba104c98ed3b4ef2edd7f3
AlexXie7 <103275372+AlexXie7@users.noreply.github.com>
2022-05-06 01:36:53 -0700

    Added mouse events

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -10,37 +10,97 @@ class Play extends Phaser.Scene {
 
     create() {
         this.map = this.add.sprite(gameCenterX, gameCenterY,'map').setDisplaySize(game.scale.width, game.scale.height);
-        this.player = this.add.follower(null, gameCenterX, gameCenterY, 'player');
-
-        this.isWalking = false;
 
         // Map Waypoints
         this.school = {
             x: game.scale.width  * 3 / 4,
             y: game.scale.height * 1 / 4,
-            minigames: []
+            minigames: [],
+            sprite: this.add.rectangle(game.scale.width  * 3 / 4, game.scale.height * 1 / 4, 150, 150, 0xffffff)
         };
 
         this.home = {
             x: gameCenterX,
             y: gameCenterY,
-            minigames: []
+            minigames: [],
+            sprite: this.add.rectangle(gameCenterX, gameCenterY, 150, 150, 0xffffff)
         };
 
         this.restaurant = {
             x: gameCenterX / 2,
             y: gameCenterY * 1.5,
-            minigames: []
+            minigames: [],
+            sprite: this.add.rectangle(gameCenterX/2, gameCenterY*1.5, 150, 150, 0xffffff)
         };
 
         //maybe we should rename town to plaza?
         this.town = {
             x: gameCenterX / 2,
             y: gameCenterY / 2,
-            minigames: []
+            minigames: [],
+            sprite: this.add.rectangle(gameCenterX/2, gameCenterY/2, 150, 150, 0xffffff)
         };
 
+        // interactivity for the town sprites
+        this.school.sprite.setInteractive();
+        this.home.sprite.setInteractive();
+        this.restaurant.sprite.setInteractive();
+        this.town.sprite.setInteractive();
+
+        // On hover over sprite
+        //we can implement effects in here later
+        this.school.sprite.on('pointerover', () => {
+            console.log('Over the School');
+        });
+        this.home.sprite.on('pointerover', () => {
+            console.log('Over the home');
+        });
+        this.restaurant.sprite.on('pointerover', () => {
+            console.log('Over the restaurant');
+        });
+        this.town.sprite.on('pointerover', () => {
+            console.log('Over the town');
+        });
+
+        // On hover off sprite
+        //we can make sure to shut off the effects here later
+        this.school.sprite.on('pointerout', () => {
+            console.log('Off the School');
+        });
+        this.home.sprite.on('pointerout', () => {
+            console.log('Off the home');
+        });
+        this.restaurant.sprite.on('pointerout', () => {
+            console.log('Off the restaurant');
+        });
+        this.town.sprite.on('pointerout', () => {
+            console.log('Off the town');
+        });
+
+        // On click release
+        // This is how we enter the minigames?
+        this.school.sprite.on('pointerup', () => {
+            console.log('Clicked the School');
+        });
+        this.home.sprite.on('pointerup', () => {
+            console.log('Clicked the home');
+        });
+        this.restaurant.sprite.on('pointerup', () => {
+            console.log('Clicked the restaurant');
+        });
+        this.town.sprite.on('pointerup', () => {
+            console.log('Clicked the town');
+        });
+
+
+        this.player = this.add.follower(null, gameCenterX, gameCenterY, 'player');
+
+        this.isWalking = false;
+        this.location = "home";
+
+
         this.walkToSchool();
+        console.log(this.school.sprite.x);
 
     }
 
@@ -66,6 +126,8 @@ class Play extends Phaser.Scene {
             //yoyo: true,
             //rotateToPath: false
         });
+
+        this.location = "school";
 
         setTimeout(() => {
             this.isWalking = false;
@@ -94,19 +156,14 @@ class Play extends Phaser.Scene {
         this.scene.stop(scene);
     }
 
-    update() {
-
-    }
-
     walkToHome() {
         let walkPath = this.add.path(this.player.x, this.player.y);
         walkPath.lineTo(this.home.x, this.home.y);
         this.isWalking = true;
-        setTimeout(() => {
-            this.isWalking = false;
-        }, 3000);
+
 
         this.player.path = walkPath;
+
         this.player.startFollow({
             from: 0,
             to: 1,
@@ -115,6 +172,12 @@ class Play extends Phaser.Scene {
             ease: 'Power0',
             hold: 0,
         });
+
+        this.location = "home";
+
+        setTimeout(() => {
+            this.isWalking = false;
+        }, 3000);
     }
 
     walkToRestaurant() {
@@ -122,9 +185,6 @@ class Play extends Phaser.Scene {
         walkPath.lineTo(this.restaurant.x, this.restaurant.y);
 
         this.isWalking = true;
-        setTimeout(() => {
-            this.isWalking = false;
-        }, 3000);
 
         this.player.path = walkPath;
         this.player.startFollow({
@@ -135,6 +195,12 @@ class Play extends Phaser.Scene {
             ease: 'Power0',
             hold: 0,
         });
+        
+        this.location = "restaurant";
+
+        setTimeout(() => {
+            this.isWalking = false;
+        }, 3000);
     }
 
     walkToTown() {
@@ -142,9 +208,7 @@ class Play extends Phaser.Scene {
         walkPath.lineTo(this.town.x, this.town.y);
 
         this.isWalking = true;
-        setTimeout(() => {
-            this.isWalking = false;
-        }, 3000);
+
 
         this.player.path = walkPath;
         this.player.startFollow({
@@ -155,6 +219,30 @@ class Play extends Phaser.Scene {
             ease: 'Power0',
             hold: 0,
         });
+
+        this.location = "town";
+
+        setTimeout(() => {
+            this.isWalking = false;
+        }, 3000);
+    }
+
+    launchMinigame(){
+        if(this.location == "town"){
+
+        }
+        
+        if(this.location == "restaurant"){
+            
+        }
+        
+        if(this.location == "school"){
+            
+        }
+        
+        if(this.location == "home"){
+            
+        }
     }
     
 }
