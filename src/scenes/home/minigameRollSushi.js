0355375049d67d149127b3a5a64ae7868f43164a
AlexXie7 <103275372+AlexXie7@users.noreply.github.com>
2022-05-12 14:00:20 -0700

    rice unrolls

--- src/scenes/home/minigameRollSushi.js
+++ src/scenes/home/minigameRollSushi.js
@@ -8,6 +8,10 @@ class minigameRollSushi extends Minigame {
 
         this.uiScene = this.scene.get('uiScene');
         this.load.image('sushiMat', './assets/minigameRollSushi/tempSushiMat.jpg');
+        this.load.image('jelly', './assets/minigameRollSushi/tempJ.jpg');
+        this.load.image('pb', './assets/minigameRollSushi/tempPB.png');
+        this.load.image('arrowUp', './assets/minigameRollSushi/tempArrowUp.png')
+        
 
     }
 
@@ -17,7 +21,28 @@ class minigameRollSushi extends Minigame {
         this.bg = this.add.sprite(gameCenterX, gameCenterY, 'sushiMat').setDisplaySize(game.scale.width, game.scale.height);
 
         this.nori = this.add.rectangle(gameCenterX, gameCenterY, game.scale.width * .7, game.scale.height * .7, '0x006400');
-        this.rice = this.add.rectangle(gameCenterX, gameCenterY * 1.5, this.nori.width - 10, this.nori.height/4, '0xFFFFFF');
+        this.rice = this.add.rectangle(gameCenterX, gameCenterY * 1.5, this.nori.width - 10, this.nori.height/4, '0xFFFFFF').setInteractive();
+
+        //pick up the whole peanut butter and dump it on the sushi
+        this.pb = this.add.sprite(0, gameCenterY, 'pb');
+        this.jelly = this.add.sprite(game.scale.width, gameCenterY, 'jelly');
+        //this.jelly.x -= this.jelly.width;
+
+        this.riceClicked = 1;
+        this.pbSpread = false;
+        this.jellySpread = false;
+
+        this.rice.on('pointerup', () => {
+            if(this.riceClicked < 4){
+                this.riceClicked += 1;
+                this.rice.y -= this.nori.height/4;
+                this.rice.height += this.nori.height/4;
+                if(this.riceClicked == 4){
+                    this.pb.setDraggable();
+                    this.jelly.setDraggable();
+                }
+            }
+        });
 
 
 
