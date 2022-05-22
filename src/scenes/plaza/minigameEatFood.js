7720079273853a6b9a6910e81f3643b5ce6f0f49
Mattowskinator <mchen146@ucsc.edu>
2022-05-22 04:10:16 -0700

    fork moves and grabs food

--- src/scenes/plaza/minigameEatFood.js
+++ src/scenes/plaza/minigameEatFood.js
@@ -11,6 +11,7 @@ class minigameEatFood extends Minigame {
         this.load.image('jaw', './assets/minigameEatFood/jaw.png');
         this.load.image('bowl', './assets/minigameEatFood/bowl.png');
         this.load.image('noodles', './assets/minigameEatFood/noodles.png');
+        this.load.image('noodleClump', './assets/minigameEatFood/noodle-clump.png');
         this.load.image('fork', './assets/minigameEatFood/fork.png');
         this.load.image('chopsticks', './assets/minigameEatFood/chopsticks.png');
     }
@@ -24,60 +25,96 @@ class minigameEatFood extends Minigame {
         this.scale = 1.5;
 
         // head stuff
-        this.head = this.add.image(gameCenterX, gameCenterY - 300 * this.scale, 'head').setOrigin(.5).setScale(this.scale);
+        this.head = this.add.image(gameCenterX, gameCenterY - 200 * this.scale, 'head').setOrigin(.5).setScale(this.scale);
         this.jaw = this.add.image(this.head.x, this.head.y, 'jaw').setOrigin(.5).setScale(this.scale);
         
         // food/bowl stuff
-        this.food = this.add.image(gameCenterX, gameCenterY, 'noodles').setOrigin(.5).setScale(this.scale);
+        this.food = this.add.image(gameCenterX, gameCenterY + 100 * this.scale, 'noodles').setOrigin(.5).setScale(this.scale);
         this.bowl = this.add.image(this.food.x, this.food.y, 'bowl').setOrigin(.5).setScale(this.scale);
+        // this.foodClump = this.add.image(gameCenterX, gameCenterY, 'noodleClump').setOrigin(.5).setScale(this.scale);
+        // this.foodClump.setVisible(false);
 
         // utensils
         this.fork = this.add.image(gameCenterX + 230 * this.scale, gameCenterY + 50 * this.scale, 'fork').setOrigin(.5).setScale(this.scale);
-        this.fork.startX = this.fork.x;
-        this.fork.startY = this.fork.y;
-        this.fork.targetX = this.fork.startX;
-        this.fork.targetY = this.fork.startY;
-        this.fork.setInteractive({
-            draggable: true
-        });
-        // this.fork.setDraggable = true;
-        this.fork.on(Phaser.Input.Events.DRAG, (pointer) => {
-            this.fork.targetX = pointer.x
-            this.fork.targetY = pointer.y
-        })
-        this.fork.on(Phaser.Input.Events.DRAG_END, (pointer) => {
-            this.fork.targetX = this.fork.startX
-            this.fork.targetY = this.fork.startY
-        })
+        this.initDraggable(this.fork);
+        
+        this.activePoint;
     }
 
     update(time, delta) {
 
-        const pointer = game.input.activePointer;
-
-        const distance = Phaser.Math.Distance.Between(pointer.x, pointer.y, this.head.x, this.head.y + 150 * this.scale);
+        // const pointer = game.input.activePointer;
+        if (this.activePoint) {
+            // distance of active point to mouth
+            const distance = Phaser.Math.Distance.Between(this.activePoint.x, this.activePoint.y, this.head.x, this.head.y + 150 * this.scale);
+            const maxDistance = 100;
+            const maxJaw = 60;
+            const jawOffset = this.scale * (Math.pow(Math.max(maxDistance - distance, 0) / maxDistance, .5)) * maxJaw;
+            this.jaw.targetY = this.head.y + jawOffset;
+        } else {
+            this.jaw.targetY = this.head.y;
+        }
+        
         //(Math.sin(time * .02) * .5 + 1) * 
 
-        const maxDistance = 100;
-        const maxJaw = 60;
-        const jawOffset = this.scale * (Math.pow(Math.max(maxDistance - distance, 0) / maxDistance, .5)) * maxJaw;
-        const targetJawY = this.head.y + jawOffset
+        this.jaw.y += (this.jaw.targetY - this.jaw.y) * delta * .006;
 
-        this.jaw.y += (targetJawY - this.jaw.y) * delta * .006;
-
-        this.fork.x += (this.fork.targetX - this.fork.x) * delta * .01;
-        this.fork.y += (this.fork.targetY - this.fork.y) * delta * .01;
-
-        // // call when the minigame has a finished condition
-        // this.finish(true); // true if success, false if failure
-
-        // // or simply set isPassed to true or false if the state of the minigame is a success or failure
-        // this.isPassed = true;
+        this.fork.update(time, delta);
     }
 
-    onTimeout() {
-        // do timeout operations ex: clear any ui stuff
+    initDraggable(object) {
+        object.startX = object.x;
+        object.startY = object.y;
+        object.targetX = object.startX;
+        object.targetY = object.startY;
+        object.setInteractive({
+            draggable: true
+        });
+        object.on(Phaser.Input.Events.DRAG, (pointer) => {
+            object.targetX = pointer.x
+            object.targetY = pointer.y
+            const point = object.getTopCenter();
+            this.activePoint = point;
+        })
+        object.on(Phaser.Input.Events.DRAG_END, (pointer) => {
+            object.targetX = object.startX
+            object.targetY = object.startY
+            this.activePoint = undefined;
+        })
+        object.update = (time, delta) => {
+            object.x += (object.targetX - object.x) * delta * .01;
+            object.y += (object.targetY - object.y) * delta * .01;
+            const point = object.getTopCenter();
+            if (!this.pointOnObject(point.x, point.y, this.bowl) && this.pointOnObject(point.x, point.y, this.food)) {
+                if (!object.food) {
+                    object.food = this.createFoodClump(point.x, point.y);
+                }
+            }
+
+            if (object.food) {
+                object.food.x = point.x;
+                object.food.y = point.y;
+            }
+            
+        }
     }
 
+    createFoodClump(x, y) {
+        const clump = this.add.image(x, y, 'noodleClump').setOrigin(.5).setScale(this.scale);
+        return clump;
+    }
 
+    // minAlpha if set should be ranged between 0 and 255
+    // does not account for rotation
+    pointOnObject(x, y, object, minAlpha = 127) {
+        const tl = object.getTopLeft();
+        const relX = (x - tl.x) / object.displayWidth;
+        const relY = (y - tl.y) / object.displayHeight;
+        const alpha = game.textures.getPixelAlpha(
+            relX * object.width,
+            relY * object.height,
+            object.texture.key, 0
+        );
+        return alpha >= minAlpha;
+    }
 }
