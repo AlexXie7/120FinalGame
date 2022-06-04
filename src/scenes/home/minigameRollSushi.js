61ca53cb1da31296e40fb3857fc7ca5f95b278c6
AlexXie7 <103275372+AlexXie7@users.noreply.github.com>
2022-06-03 23:32:30 -0700

    updated make sushi

--- src/scenes/home/minigameRollSushi.js
+++ src/scenes/home/minigameRollSushi.js
@@ -3,52 +3,62 @@ class minigameRollSushi extends Minigame {
         super(id);
     }
 
+
+// copy to create()
+
+
+
     preload() {
         super.preload();
 
-        this.load.image('sushiMat', './assets/minigameRollSushi/tempSushiMat.jpg');
-        this.load.image('jelly', './assets/minigameRollSushi/tempJ.jpg');
-        this.load.image('pb', './assets/minigameRollSushi/tempPB.png');
-        this.load.image('arrowUp', './assets/minigameRollSushi/tempArrowUp.png')
+        this.load.image('arrowUp', './assets/minigameRollSushi/tempArrowUp.png');
         
+        this.load.image('sushiMat', '/assets/minigameRollSushi/SushiMat.png');
+        this.load.image('seaweed', '/assets/minigameRollSushi/Seaweed.png');
+        this.load.spritesheet('rice', '/assets/minigameRollSushi/riceSprite.png', {frameWidth: 620, frameHeight: 371});
+        this.load.image('pb', '/assets/minigameRollSushi/PeanutButter.png');
+        this.load.image('jelly', '/assets/minigameRollSushi/Jelly.png');
+        this.load.image('jellySpread', '/assets/minigameRollSushi/jellySpread.png');
+        this.load.image('pbSpread', '/assets/minigameRollSushi/pbSpread.png');
     }
 
     create() {
         super.create();
 
+        this.sushiMat = this.add.image(600, 450, 'sushiMat').setOrigin(0.5, 0.5).setScale(1, 1);
+        this.nori = this.add.image(600, 450, 'seaweed').setOrigin(0.5, 0.5).setScale(1, 1);
+        this.rice = this.add.sprite(600, 475, 'rice').setOrigin(0.5, 0.5).setScale(1, 1).setInteractive();
+        this.pb = this.add.image(120, 700, 'pb').setOrigin(0.5, 0.5).setScale(1, 1).setInteractive().setAlpha(0);
+        this.jelly = this.add.image(1080, 700, 'jelly').setOrigin(0.5, 0.5).setScale(1, 1).setInteractive().setAlpha(0);
+        this.jellySmear = this.add.image(600, 450, 'jellySpread').setOrigin(0.5, 0.5).setScale(1, 1).setAlpha(0);
+        this.pbSmear = this.add.image(600, 450, 'pbSpread').setOrigin(0.5, 0.5).setScale(1, 1).setAlpha(0);
+
         // this.uiScene = this.scene.get('uiScene');
         this.uiScene.setInstructions('Spread Rice!');
-
-        this.bg = this.add.sprite(gameCenterX, gameCenterY, 'sushiMat').setDisplaySize(game.scale.width, game.scale.height);
-
-        this.nori = this.add.rectangle(gameCenterX, gameCenterY, game.scale.width * .7, game.scale.height * .7, '0x006400');
-        this.rice = this.add.rectangle(gameCenterX, gameCenterY * 1.5, this.nori.width - 10, this.nori.height/4, '0xFFFFFF').setInteractive();
 
         this.arrow = this.add.sprite(gameCenterX, gameCenterY*2, 'arrowUp').setAlpha(0).setScale(.1);
         this.arrow.y -= this.arrow.height;
 
-        //pick up the whole peanut butter and dump it on the sushi
-        this.pb = this.add.sprite(0, gameCenterY, 'pb').setInteractive().setAlpha(0);
-        this.jelly = this.add.sprite(game.scale.width, gameCenterY, 'jelly').setInteractive().setAlpha(0);
         //this.jelly.x -= this.jelly.width;
 
-        this.riceClicked = 1;
+        this.riceClicked = 0;
         this.arrowClicked = false;
         this.pbSpread = false;
         this.jellySpread = false;
 
         this.rice.on('pointerdown', () => {
-            if(this.riceClicked < 3){
+            if(this.riceClicked < 2){
                 this.riceClicked += 1;
-                this.rice.y -= this.nori.height/4;
-                this.rice.height += this.nori.height/4;
-                if(this.riceClicked == 3){
+                if(this.riceClicked <= 2) {
+                    this.rice.setFrame(this.riceClicked);
+                }
+                if(this.riceClicked == 2){
                     console.log("draggable");
                     this.uiScene.setInstructions("Spread PB&J");
                     this.pb.setAlpha(1);
                     this.jelly.setAlpha(1);
                     this.input.setDraggable([this.pb, this.jelly]);
-                }
+                } 
             }
         });
 
@@ -65,6 +75,7 @@ class minigameRollSushi extends Minigame {
 
                     this.pb.destroy();
                     this.pbSpread = true;
+                    this.pbSmear.setAlpha(.7);
 
                     if(this.jellySpread){
                         this.arrow.setAlpha(.7);
@@ -81,6 +92,8 @@ class minigameRollSushi extends Minigame {
 
                     this.jelly.destroy();
                     this.jellySpread = true;
+                    this.jellySmear.setAlpha(.7);
+
 
                     if(this.pbSpread){
                         this.arrow.setAlpha(.7);
